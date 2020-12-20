376bc39bda6610ae05cf1c3b4da4ffbf24484eca
Stephen <smith_sra@yahoo.co.uk>
2020-12-20 21:37:21 +0000

    First upload

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '89f6e42d763941b9a88857aecb8d1844'
+});
+
+const handleApiCall = (req, res) => {
+app.models
+	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	.then(data => {
+		res.json(data);
+	})
+	.catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, mydatabase) => {
+	const { id } = req.body;
+	mydatabase('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require ('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const mydatabase = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'stephen',
+    password : 'NewY0rk!',
+    database : 'smart-brain'
+  }
+});
+
+// mydatabase.select('*').from('users').then(data => {
+// 	console.log(data);
+// })
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// const database = {
+// 	users: [
+// 		{
+// 			id:'123',
+// 			name:'John',
+// 			password: 'cookies',
+// 			email: 'john@gmail.com',
+// 			entries: 0,
+// 			joined: new Date()
+// 		},
+// 		{
+// 			id:'124',
+// 			name:'Sally',
+// 			password: 'bananas',
+// 			email: 'sally@gmail.com',
+// 			entries: 0,
+// 			joined: new Date()
+// 		}
+// 	],
+// 	login: [
+// 		{
+// 			id: '987',
+// 			hash: '',
+// 			email: 'john@gmail.com'
+// 		}
+// 	]
+// }
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, mydatabase, bcrypt) })
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, mydatabase, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, mydatabase) })
+
+app.put('/image', (req, res) => {image.handleImage(req, res, mydatabase) })
+
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res) })
+
+// app.put('/image', (req, res) => {
+// 	const { id } = req.body;
+// 	mydatabase('users').where('id', '=', id)
+// 	.increment('entries', 1)
+// 	.returning('entries')
+// 	.then(entries => {
+// 		res.json(entries[0]);
+// 	})
+// 	.catch(err => res.status(400).json('unable to get entries'))
+// })
+
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//     // Store hash in your password DB.
+// });
+
+
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+}) 
